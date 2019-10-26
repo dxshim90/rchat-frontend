b90ed0e58e3b434fbad2fd9a6fdd6f48ff2ccafa
dxshim90 <dxshim90@gmail.com>
2019-10-26 18:24:08 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//import Pages
+import Welcome from "./Pages/Welcome/Welcome";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import ChatPage from "./Pages/ChatPage/ChatPage";
+import ProfilePage from "./Pages/Profile/ProfilePage";
+import Shop from "./Pages/Shop/Shop";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/chat/:id" component={ChatPage} />
+          <Route exact path="/profile/:id" component={ProfilePage} />
+          <Route exact path="/shop" component={Shop} />
+        </Switch>
+      </Router>
     </div>
   );
 }
