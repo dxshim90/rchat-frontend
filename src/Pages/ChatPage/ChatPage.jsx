ef7287bc481cac279cacf3acc50741de70597635
dxshim90 <dxshim90@gmail.com>
2019-11-17 20:22:43 +0000

    example chat page created

--- src/Pages/ChatPage/ChatPage.jsx
+++ src/Pages/ChatPage/ChatPage.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React from 'react';
+import { Container, Row, Col, Button } from 'reactstrap';
 
 const ChatPage = () => {
-  return <div>Chat Page</div>;
+  return (
+    <Container fluid>
+      <Row style={{ marginTop: '2rem' }}>
+        <Col>
+          <h2>Chat with ....</h2>
+        </Col>
+      </Row>
+      <Row style={{ marginTop: '2rem' }}>
+        <div style={{ width: '10%' }}></div>
+        <Col
+          style={{
+            backgroundColor: 'pink',
+            height: '60vh',
+            border: '1px solid black'
+          }}
+        >
+          Chat Box
+        </Col>
+        <div style={{ width: '10%' }}></div>
+      </Row>
+      <Row>
+        <div style={{ width: '10%' }}></div>
+        <Col style={{ padding: '0' }}>
+          <div style={{ display: 'flex' }}>
+            <Button color="success">Upload A File</Button>
+            <input
+              style={{ width: '100%' }}
+              type="text"
+              placeholder="type a message here"
+            />
+            <Button color="success">Send Message</Button>
+          </div>
+        </Col>
+        <div style={{ width: '10%' }}></div>
+      </Row>
+    </Container>
+  );
 };
 
 export default ChatPage;
